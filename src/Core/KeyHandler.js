2b0c3d782858f62a0f960451baade5c8774ba817
Olezhich <qwertypx@yandex.ru>
2025-11-01 14:49:02 +0300

    added sqrt logic

--- src/Core/KeyHandler.js
+++ src/Core/KeyHandler.js
@@ -27,9 +27,10 @@ export function KeyHandler({prev, key, SelfState}){
         return ({register: null, state: {...SelfState, FunctionalMode: true}});;
     }
     res.mantissa = res.mStr === '' ? 0 : Number(res.mStr);
+    res.dStr = (res.dStr === '0' || res.dStr === '-0' ? '' : res.dStr);
     res.degree = res.dStr === '' ? 0 : Number(res.dStr);
     res.mOverflow = IsOverflow(res.mantissa);
-
+    console.log('KeyHandler: ',res.mStr, res.dStr);
     const state = {...SelfState, FunctionalMode: false};
     return({register: res, state});
 }
@@ -40,12 +41,12 @@ function NumberHandler({prev, key, res}){
     else if(!prev.inputDegree)
         res.mStr = prev.mStr + key;
     else if(Math.abs(prev.degree) * 10 > 90){
-        if(key === '0')
-            return null;
+        // if(key === '0')
+        //     return null;
         res.dStr = (prev.dStr.startsWith('-') ? '-' : '') + key;
     }
-    else if(prev.dStr === '' && key === '0')
-        return null;
+    // else if(prev.dStr === '' && key === '0')
+    //     return null;
     else{
         res.dStr = prev.dStr + key;
     }   
@@ -54,10 +55,35 @@ function NumberHandler({prev, key, res}){
 function FunctionHandler({prev, key, res}){
     let newMantissa;
     let newDegree;
-    if(key === '9'){ //sqrt
-        newMantissa = Math.sqrt(prev.mantissa);
-        newDegree = 
-        console.log(newMantissa);
+    let degree = prev.degree;
+    let mantissa = prev.mantissa;
+    if(key === '6'){ //sqrt
+        if(prev.degree%2 === 1){
+            degree -= 1;
+            mantissa *= 10;
+        }
+        newMantissa = Math.sqrt(mantissa);
+        newDegree = degree / 2;
+        console.log('SQRT func: ',newMantissa, newDegree);
+        while(newMantissa < 100_000_000 && newDegree > 0){
+            newDegree -= 1;
+            newMantissa *= 10;
+        }
+        while(String(newMantissa).length < 9  && newDegree < 0){ 
+            newDegree += 1;
+            newMantissa /= 10;
+        }
+        if(String(newMantissa).length > 9){
+            while(newMantissa > 10){
+                newDegree += 1;
+            newMantissa /= 10;
+            }
+        }
+        res.dStr = String(newDegree);
+        newMantissa = String(newMantissa.toFixed(7));
+        res.mStr = (newMantissa.includes('.')? newMantissa.slice(0,9):newMantissa.slice(0,8));
+        
+        console.log('SQRT func: ',newMantissa, newDegree, res.mStr, res.dStr);
         //res.dStr = String(prev.degree / 2);
     }
 }
