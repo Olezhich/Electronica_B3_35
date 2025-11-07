edba22e68a7c3bb2f34c85c394620f2b01744ab9
Olezhich <qwertypx@yandex.ru>
2025-11-07 09:12:13 +0300

    display handler exponential notation fix

--- src/Core/KeyHandler.js
+++ src/Core/KeyHandler.js
@@ -65,20 +65,24 @@ function FunctionHandler({prev, key, res}){
         newMantissa = Math.sqrt(mantissa);
         newDegree = degree / 2;
         console.log('SQRT func: ',newMantissa, newDegree);
-        while(newMantissa < 100_000_000 && newDegree > 0){
-            newDegree -= 1;
-            newMantissa *= 10;
-        }
-        while(String(newMantissa).length < 9  && newDegree < 0){ 
-            newDegree += 1;
-            newMantissa /= 10;
-        }
-        if(String(newMantissa).length > 9){
-            while(newMantissa > 10){
-                newDegree += 1;
-            newMantissa /= 10;
-            }
-        }
+        // while(newMantissa < 100_000_000 && newDegree > 0){
+        //     newDegree -= 1;
+        //     newMantissa *= 10;
+        // }
+        // while(String(newMantissa).startsWith('0')  && newDegree < 0){ 
+        //     newDegree += 1;
+        //     newMantissa /= 10;
+        // }
+        // while(String(newMantissa).length < 9){
+        //     newDegree += 1;
+        //     newMantissa /= 10;
+        // }
+        // if(String(newMantissa).length > 9){
+        //     while(newMantissa > 10){
+        //         newDegree += 1;
+        //     newMantissa /= 10;
+        //     }
+        // }
         res.dStr = String(newDegree);
         newMantissa = String(newMantissa.toFixed(7));
         res.mStr = (newMantissa.includes('.')? newMantissa.slice(0,9):newMantissa.slice(0,8));
